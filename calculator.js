a978399af2569d86af773defe660a7947ba1133d
Rahul Vanmali <rahul@nextbillion.ai>
2025-05-26 22:27:16 +0530

    feat: custom delimitor added

--- calculator.js
+++ calculator.js
@@ -1,9 +1,19 @@
+function escapeRegex(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
+
 
 function add(stringValue){
     if(!stringValue || stringValue.trim() == "") return 0
     let numbers = ""+stringValue.trim();
     let delimiter = /,|\n/; // Default delimiters: comma or newline
-
+    // Handle custom delimiter syntax
+    if (numbers.startsWith('//')) {
+        const delimiterLineEnd = numbers.indexOf('\n');
+        const rawDelimiter = numbers.slice(2, delimiterLineEnd);
+        delimiter = new RegExp(escapeRegex(rawDelimiter));
+        numbers = numbers.slice(delimiterLineEnd + 1);
+      }
 
     // Split the input string by the delimiter(s) "," basic
     const parts = numbers.split(delimiter).map(n => n.trim()).filter(n => n !== '');
--- calculator.test.js
+++ calculator.test.js
@@ -15,10 +15,23 @@ describe("String Calculator - add()", () => {
       expect(add("1,2,3,4,5")).toBe(15);
     });
   });
+  //   new line delimiter
   describe("Supports new lines as delimiters", () => {
     test("returns the sum when newlines are mixed with commas", () => {
       expect(add("1\n2,3")).toBe(6);
       expect(add("10\n20\n30")).toBe(60);
     });
-  })
+  });
+
+  describe("Supports custom delimiters", () => {
+    test('supports custom delimiter syntax "//;\\n1;2"', () => {
+      expect(add("//;\n1;2")).toBe(3);
+    });
+    test('supports custom delimiter syntax "//;\\n1;2"', () => {
+        expect(add("//*\n1*2")).toBe(3);
+      });
+      test('supports custom delimiter syntax "//`\\n1`2"', () => {
+        expect(add("//`\n2`2")).toBe(4);
+      });
+  });
 });
