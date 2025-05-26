ebdfc4f5a60e3401a73e7b6d1c52c42a611363d8
Rahul Vanmali <rahul@nextbillion.ai>
2025-05-26 22:21:21 +0530

    feat: new line delimitor added

--- calculator.js
+++ calculator.js
@@ -1,5 +1,18 @@
 
 function add(stringValue){
     if(!stringValue || stringValue.trim() == "") return 0
+    let numbers = ""+stringValue.trim();
+    let delimiter = /,|\n/; // Default delimiters: comma or newline
+
+
+    // Split the input string by the delimiter(s) "," basic
+    const parts = numbers.split(delimiter).map(n => n.trim()).filter(n => n !== '');
+
+    const sum = parts.reduce((total, numStr) => {
+        const num = parseInt(numStr, 10);
+        return total + num;
+      }, 0);
+      return sum;
+
 }
 module.exports = {add}
--- calculator.test.js
+++ calculator.test.js
@@ -1,7 +1,24 @@
-const { add } = require("./calculate");
-
-    describe("Basic cases", () => {
-        test("returns 0 for an empty string", () => {
-          expect(add("")).toBe(0);
-        });
-})
+const { add } = require("./calculator");
+describe("String Calculator - add()", () => {
+  describe("Basic cases", () => {
+    test("returns 0 for an empty string", () => {
+      expect(add("")).toBe(0);
+    });
+    test("returns the number itself for a single number", () => {
+      expect(add("1")).toBe(1);
+      expect(add("42")).toBe(42);
+    });
+    test("returns the sum for two numbers separated by comma", () => {
+      expect(add("1,5")).toBe(6);
+    });
+    test("returns the sum for multiple comma-separated numbers", () => {
+      expect(add("1,2,3,4,5")).toBe(15);
+    });
+  });
+  describe("Supports new lines as delimiters", () => {
+    test("returns the sum when newlines are mixed with commas", () => {
+      expect(add("1\n2,3")).toBe(6);
+      expect(add("10\n20\n30")).toBe(60);
+    });
+  })
+});
